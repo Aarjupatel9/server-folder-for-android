23a6814f7109887ad79f76a0a895fbafb1525c65
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-05-24 12:08:50 +0530

    socket testing

--- server-socket.js
+++ server-socket.js
@@ -1039,7 +1039,7 @@ async function SocketCommunicationMassegeSend(url, data) {
 
 app.get("/removeMassege", async (req, res) => {
   const time = req.query.time;
-  console.log(" API || /removeMassege || parameter is ",time);
+  console.log(" API || /removeMassege || parameter is ", time);
   const result = await DbO.collection("masseges").updateMany(
     { _id: ObjectId("646094f995ce9ebfa09c968c") },
     {
@@ -1049,7 +1049,7 @@ app.get("/removeMassege", async (req, res) => {
         },
       },
     },
-    { arrayFilters: [] }
+    { arrayFilters: [{}] }
   );
 
   if (result.modifiedCount > 0) {
