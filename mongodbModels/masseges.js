591a35857ed4449e7a0cdaa72b5f3b9347d3cc55
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-06-29 19:36:49 +0530

    For debug

--- mongodbModels/masseges.js
+++ mongodbModels/masseges.js
@@ -60,7 +60,64 @@
 
 // module.exports = mongoose.model("massege", massegeSchema);
 
+// const mongoose = require("mongoose");
+// const massegeHolderSchema = new mongoose.Schema({
+//   from: {
+//     type: String,
+//     required: true,
+//   },
+//   to: {
+//     type: String,
+//     required: true,
+//   },
+//   massege: {
+//     type: String,
+//     required: true,
+//   },
+//   chatId: {
+//     type: String,
+//     required: true,
+//   },
+//   time: {
+//     type: Number,
+//     required: true,
+//     unique: true,
+//   },
+//   massegeStatus: {
+//     type: Number,
+//     required: true,
+//   },
+//   massegeStatusL: {
+//     type: Number,
+//     required: true,
+//   },
+//   ef1: {
+//     type: Number,
+//     required: true,
+//   },
+//   ef2: {
+//     type: Number,
+//     required: true,
+//   },
+// });
+
+// const massegeSchema = new mongoose.Schema({
+//   user1: {
+//     type: String,
+//     required: true,
+//   },
+//   user2: {
+//     type: String,
+//     required: true,
+//   },
+//   massegeHolder: [massegeHolderSchema],
+// });
+
+// module.exports = mongoose.model("massege", massegeSchema);
+
+
 const mongoose = require("mongoose");
+
 const massegeHolderSchema = new mongoose.Schema({
   from: {
     type: String,
@@ -81,7 +138,6 @@ const massegeHolderSchema = new mongoose.Schema({
   time: {
     type: Number,
     required: true,
-    unique: true,
   },
   massegeStatus: {
     type: Number,
@@ -110,7 +166,20 @@ const massegeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  massegeHolder: [massegeHolderSchema],
+  massegeHolder: {
+    type: [massegeHolderSchema],
+    validate: {
+      validator: function (arr) {
+        // Validate uniqueness of 'time' within the array
+        const uniqueTimes = new Set(arr.map((obj) => obj.time));
+        return uniqueTimes.size === arr.length;
+      },
+      message: "Duplicate 'time' values found in massegeHolder array",
+    },
+  },
 });
 
+massegeSchema.index({ "massegeHolder.time": 1 }, { unique: true });
+
 module.exports = mongoose.model("massege", massegeSchema);
+
