83a6d3f63a16528d9e7e55116f168c2fca3787fd
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-06-29 19:32:08 +0530

    For debug

--- mongodbModels/masseges.js
+++ mongodbModels/masseges.js
@@ -1,4 +1,105 @@
+// const mongoose = require("mongoose");
+
+// const massegeSchema = new mongoose.Schema({
+//   user1: {
+//     type: String,
+//     required: true,
+//   },
+//   user2: {
+//     type: String,
+//     require: true,
+//   },
+//   massegeHolder: [
+//     {
+//       from: {
+//         type: String,
+//         required: true,
+//       },
+//       to: {
+//         type: String,
+//         required: true,
+//       },
+//       massege: {
+//         type: String,
+//         required: true,
+//       },
+//       chatId: {
+//         type: String,
+//         required: true,
+//       },
+//       time: {
+//         type: Number,
+//         required: true,
+//         unique: true,
+//       },
+//       massegeStatus: {
+//         type: Number,
+//         required: true,
+//       },
+//       massegeStatusL: {
+//         type: Number,
+//         required: true,
+//       },
+//       ef1: {
+//         type: Number,
+//         required: true,
+//       },
+//       ef2: {
+//         type: Number,
+//         required: true,
+//       },
+//     },
+//   ],
+// });
+
+// // massegeSchema.plugin(uniqueValidator, {
+// //   message: "Error, {PATH} must be unique.",
+// // });
+
+// // massegeSchema.index({ user1: 1, user2: 1 }, { unique: true });
+
+// module.exports = mongoose.model("massege", massegeSchema);
+
 const mongoose = require("mongoose");
+const massegeHolderSchema = new mongoose.Schema({
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true,
+  },
+  massege: {
+    type: String,
+    required: true,
+  },
+  chatId: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  massegeStatus: {
+    type: Number,
+    required: true,
+  },
+  massegeStatusL: {
+    type: Number,
+    required: true,
+  },
+  ef1: {
+    type: Number,
+    required: true,
+  },
+  ef2: {
+    type: Number,
+    required: true,
+  },
+});
 
 const massegeSchema = new mongoose.Schema({
   user1: {
@@ -7,55 +108,9 @@ const massegeSchema = new mongoose.Schema({
   },
   user2: {
     type: String,
-    require: true,
+    required: true,
   },
-  massegeHolder: [
-    {
-      from: {
-        type: String,
-        required: true,
-      },
-      to: {
-        type: String,
-        required: true,
-      },
-      massege: {
-        type: String,
-        required: true,
-      },
-      chatId: {
-        type: String,
-        required: true,
-      },
-      time: {
-        type: Number,
-        required: true,
-        unique: true,
-      },
-      massegeStatus: {
-        type: Number,
-        required: true,
-      },
-      massegeStatusL: {
-        type: Number,
-        required: true,
-      },
-      ef1: {
-        type: Number,
-        required: true,
-      },
-      ef2: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
+  massegeHolder: [massegeHolderSchema],
 });
-
-// massegeSchema.plugin(uniqueValidator, {
-//   message: "Error, {PATH} must be unique.",
-// });
-
-// massegeSchema.index({ user1: 1, user2: 1 }, { unique: true });
 
 module.exports = mongoose.model("massege", massegeSchema);
