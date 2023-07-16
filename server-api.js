7ebb5496058db3c1ee9918b67c9b1a807371beb2
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-07-16 14:11:19 +0530

    after add self-pull-script

--- server-api.js
+++ server-api.js
@@ -60,20 +60,20 @@ const validateApiKey = (req, res, next) => {
 };
 
 // // Load the SSL certificate and key
-const privateKey = fs.readFileSync("./ssl/server.key", "utf8");
-const certificate = fs.readFileSync("./ssl/server.crt", "utf8");
+// const privateKey = fs.readFileSync("./ssl/server.key", "utf8");
+// const certificate = fs.readFileSync("./ssl/server.crt", "utf8");
 
 
 // const privateKey = fs.readFileSync("./ssl/key.pem");
 // const certificate = fs.readFileSync("./ssl/cert.pem");
 
-
-const credentials = { key: privateKey, cert: certificate };
-const server = https.createServer(credentials, app);
+// 
+// const credentials = { key: privateKey, cert: certificate };
+// const server = https.createServer(credentials, app);
 
 
 const port_api = process.env.API_PORT;
-server.listen(port_api, function () {
+app.listen(port_api, function () {
   console.log("Server-api listening at port %d", port_api);
 });
 
