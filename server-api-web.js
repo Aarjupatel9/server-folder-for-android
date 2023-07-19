815469e9ea039803a619d376f2346e52edaafafa
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-07-20 02:51:23 +0530

    after recovery email addittion

--- server-api-web.js
+++ server-api-web.js
@@ -95,34 +95,39 @@ app.post("/loginForWeb", urlencodedparser, async (req, res) => {
   if (credential.web) {
     const result = await loginModel.findOne({ Number: credential.number });
 
-    console.log("jwt secret is : ", process.env.JWT_SECRET);
+    console.log("jwt secret is : ", process.env.JWT_SECRET, " , ", process.env.JWT_EXPIRES_IN, " , ", process.env.JWT_COOKIE_EXPIRES);
     console.log("jwt secret is : ", result._id);
     if (result) {
       if (result.Password == encrypt(credential.password)) {
-        // const token = jwt.sign({ _id: result._id }, process.env.JWT_SECRET, {
-        //   expiresIn: "12h",
+   
+        // const token = jwt.sign({ id: result._id }, process.env.JWT_SECRET, {
+        //   expiresIn: "12H",
         // });
+
+        // console.log("token is :", token);
 
         // res.cookie("token", token, {
-        //   expires: new Date(Date.now() + 1 * 24 * 3600 * 1000),
+        //   expires:new Date(Date.now() + 1 * 24 * 3600 * 1000),
+        //   // maxAge: 604800000,
         //   httpOnly: true,
-        //   sameSite: "none",
-        //   secure: "true",
+        //   // sameSite: "none",
+        //   // secure: "true",
         // });
 
-        const token = jwt.sign({ id: result._id }, process.env.JWT_SECRET, {
-          expiresIn: "12H",
+        var _id = result._id;
+
+        const token = jwt.sign({ _id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRES_IN,
         });
-
-        console.log("token is :", token);
-
-        res.cookie("token", token, {
-          expires:new Date(Date.now() + 1 * 24 * 3600 * 1000),
-          // maxAge: 604800000,
+        console.log("The token is: " + token);
+        const cookieOptions = {
+          expires: new Date(
+            Date.now() +
+            process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+          ),
           httpOnly: true,
-          // sameSite: "none",
-          // secure: "true",
-        });
+        };
+        res.cookie("jwt", token, cookieOptions);
 
         res.send({ status: 1, data: result, token: token });
       } else {
