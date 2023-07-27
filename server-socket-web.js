36fff89f5fd72d331bd5e59cda51549a5b786c63
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-07-27 19:53:16 +0530

    testing

--- server-socket-web.js
+++ server-socket-web.js
@@ -36,6 +36,17 @@ app.use((req, res, next) => {
 var http = require("http").Server(app);
 var io = socketLib(http);
 
+io.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,
+}))
+io.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 
 const mongoose = require("mongoose");
