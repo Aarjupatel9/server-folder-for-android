640c6e47d8fae53de8919c921f65682882a092f3
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-07-27 19:41:13 +0530

    testing

--- server-socket-web.js
+++ server-socket-web.js
@@ -19,9 +19,9 @@ const port = process.env.WEB_SOCKET_PORT;
 const encrypt = require("./module/vigenere_enc.js");
 const decrypt = require("./module/vigenere_dec.js");
 
-app.use(cors());
+app.use(cors({ origin: "https://localhost:3000" }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
