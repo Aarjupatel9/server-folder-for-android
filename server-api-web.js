93e7bf08989f2037395b115e475867e41a49106b
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-07-31 12:58:47 +0530

    testing

--- server-api-web.js
+++ server-api-web.js
@@ -116,11 +116,10 @@ app.post("/getContactsMasseges", authenticateToken, urlencodedparser, async (req
     res.send({ status: 0 });
   }
 
-  console.log("/getContactsMasseges || start-b", id," , contacts l : ", contacts.length);
+  console.log("/getContactsMasseges || start-b", id, " , contacts l : ", contacts.length);
 
   var masseges = [];
-  await contacts.forEach(async (contact) => {
-
+  for (const contact of contacts) {
     const result = await massegesModel.findOne({
       $or: [
         {
@@ -133,15 +132,13 @@ app.post("/getContactsMasseges", authenticateToken, urlencodedparser, async (req
         },
       ],
     }, { massegeHolder: 1 });
-
     if (result) {
       masseges[contact._id] = result.massegeHolder;
       console.log("/getContactsMasseges || masseges can not be found for contact : ", contact._id, " , l : ", result.massegeHolder.length);
     } else {
       console.log("/getContactsMasseges || masseges can not be found for contact : ", contact._id);
     }
-
-  });
+  }
 
   console.log("/getContactsMasseges || masseges : ", masseges.length);
   res.send({ status: 1, masseges: masseges });
