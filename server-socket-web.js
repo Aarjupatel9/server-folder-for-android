b8dd8a57b7ce865f97e613118a255a868fd1481b
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-08-16 13:06:08 +0530

    web profile image

--- server-socket-web.js
+++ server-socket-web.js
@@ -459,15 +459,11 @@ io.on("connection", (socket) => {
     }
   });
   socket.on("contact_massege_typing_event", async function (userId, CID) {
-    // console.log("contact_massege_typing_event for CID : ", CID);
     if (CID == "-1") {
       return;
     }
-
     if (isClientConnected(CID)) {
-
       socket_local_client_instacnce.emit("sendEmitEvent", "contact_massege_typing_event", CID, getClientSocketId(CID), userId, CID); // first 3 args is fixed and other taken as array
-
     } else {
       console.log("contact_massege_typing_event || isClientConnected  false");
     }
@@ -495,6 +491,24 @@ io.on("connection", (socket) => {
     );
     console.log("updateUserDisplayName || result", result);
     socket.emit("updateUserDisplayName_return", 1);
+  });
+  socket.on("updateUserProfileImage", async function (user_id, imageData) {
+    const result = await userModel.updateOne(
+      {
+        _id: ObjectId(user_id),
+      },
+      {
+        $set: { ProfileImage: imageData },
+        $inc: { ProfileImageVersion: 1 },
+      }
+    );
+    console.log("updateUserProfileImage || result", result.modifiedCount);
+    socket.emit("updateUserProfileImage_return", 1);
+
+    const bucketName = process.env.AWS_PROFILE_IMAGE_BUCKET_NAME;
+    const imageName = user_id + ".jpg"; // Change this to your desired image name
+    const imageLink = await uploadByteArrayToS3(bucketName, imageName, imageData);
+    console.log('Image uploaded to S3. Public URL:', imageLink);
   });
 
   socket.on(
