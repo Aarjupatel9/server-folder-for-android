c07ba40b3d345273d3b76267aa1d0f650bf06c03
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-09-15 00:52:10 +0530

    log testing

---
+++ server-api-web.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// const  UploadByteArrayToS3  = require("./module/UploadByteArrayToS3");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ limit: "10000kb", extended: true }));
+
+const { getContactsList, getContactsMasseges } = require("./controllers/contactController");
+const { loginForWeb } = require("./controllers/authController");
+const { profile_displayName, profile_aboutInfo, profile_profileImage } = require("./controllers/userController");
+const { validateApiKey } = require("./middleWares/validateApiKey")
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+// const fs = require("fs");
+
+// var access = fs.createWriteStream(__dirname + '/logFiles/node.access.log', { flags: 'a' })
+// var error = fs.createWriteStream(__dirname + '/logFiles/node.error.log', { flags: 'a' });
+
+// // redirect stdout / stderr
+// // proc.stdout.pipe(access);
+// proc.stderr.pipe(error);
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/logFiles/startaw_debug.log', { flags: 'w' });
+var log_error_file = fs.createWriteStream(__dirname + '/logFiles/startaw_error.log', { flags: 'w' });
+var log_stdout = process.stdout;
+var log_stderr = process.stderr;
+
+console.log = function (d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+console.error = function (d) { //
+  log_error_file.write(util.format(d) + '\n');
+  log_stderr.write(util.format(d) + '\n');
+};
+
+
+
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://localhost:3000',
+    'http://localhost:3000',
+    'https://3.109.184.63',
+    'http://3.109.184.63',
+    'https://35.154.246.182',
+    "http://35.154.246.182"
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    console.log("origin is set to  : ", origin);
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(
+  cors({
+    origin: ['https://localhost:3000',
+      'http://localhost:3000',
+      'https://3.109.184.63',
+      'http://3.109.184.63',
+      'https://35.154.246.182',
+      "http://35.154.246.182"],
+    credentials: true,
+  })
+);
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const https = require("https");
+
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const authenticateToken = require("./module/authenticateToken")
+
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((responce) => {
+    console.log("Connected to MongoDB , ", responce.connection.name);
+  })
+  .catch((err) => console.log("err in database connection"));
+
+const loginModel = require("./mongodbModels/loginInfo");
+const userModel = require("./mongodbModels/userInfo");
+const massegesModel = require("./mongodbModels/masseges");
+
+
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json({ limit: "10000kb" }));
+
+
+const port_api = process.env.WEB_API_PORT;
+app.listen(port_api, function () {
+  console.log("Server-api listening at port %d", port_api);
+});
+
+setInterval(async function () {
+  console.log("mongodb reset");
+  const result = await loginModel.findById("64605c936952931335caeb15");
+  console.log("result in mongodb connection reset :", result);
+}, 900000);
+
+
+app.get("/", urlEncodedParser, async (req, res) => {
+  res.send({ status: 1 });
+});
+
+//for massenger-web
+app.post("/getContactsList", urlEncodedParser, validateApiKey, getContactsList);
+
+app.post("/getContactsMasseges", urlEncodedParser, getContactsMasseges);
+
+app.post("/loginForWeb", urlEncodedParser, loginForWeb);
+
+app.post("/profile/displayName", urlEncodedParser, profile_displayName);
+
+app.post("/profile/aboutInfo", urlEncodedParser, profile_aboutInfo);
+
+app.post("/profile/profileImage", urlEncodedParser, profile_profileImage);
+
