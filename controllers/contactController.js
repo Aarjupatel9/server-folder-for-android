06a190b50245243269feabadf7e8a24b7f73c27e
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-09-14 12:06:23 +0530

    block feature testing

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -8,7 +8,7 @@ exports.getContactsList = async (req, res) => {//authenticateToken,
     try {
         const id = req.body.id;
         console.log("getContactsList || start-b", id);
-        const result = await userModel.findOne({ _id: ObjectId(id) }, { Contacts: 1 }).populate({ path: "Contacts._id", select: "ProfileImageVersion " });
+        const result = await userModel.findOne({ _id: id }, { Contacts: 1 }).populate({ path: "Contacts._id", select: "ProfileImageVersion " });
         // const result = await userModel.findOne({ _id: ObjectId(id) }, { Contacts: 1 }).populate('Contacts._id');
         console.log("contacts : ", result.Contacts);
         res.send({ status: 1, contacts: result.Contacts });
