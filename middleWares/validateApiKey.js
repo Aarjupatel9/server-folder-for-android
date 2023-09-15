21f52d6c409ac6f6ae728657b4cb3bb254ef28b0
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-09-15 15:03:01 +0530

    api valicdation

--- middleWares/validateApiKey.js
+++ middleWares/validateApiKey.js
@@ -9,10 +9,10 @@ var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/../logFiles/validateApiKey_debug.log', { flags: 'w' });
 var log_stdout = process.stdout;
 
-console.log = function (d) { //
-    log_file.write(util.format(d) + '\n');
-    log_stdout.write(util.format(d) + '\n');
-};
+// console.log = function (d) { //
+//     log_file.write(util.format(d) + '\n');
+//     log_stdout.write(util.format(d) + '\n');
+// };
 
 const validApiKeys = [];
 validApiKeys.push(process.env.API_SERVER_API_KEY);
