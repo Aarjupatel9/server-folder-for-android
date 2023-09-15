9826710a7dd2e48ec8089592f7f1f72b8f47734b
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-09-15 14:16:03 +0530

    block feature testing

--- stopServers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# List all tmux sessions
-tmux_list=$(tmux list-sessions -F "#{session_name}")
-
-# Loop through each session and kill it
-for session in $tmux_list; do
-  tmux kill-session -t "$session"
-done
