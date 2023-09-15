692207609364b938de208e8dc17244adfba3aa1f
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-09-15 14:14:26 +0530

    block feature testing

--- startServers.sh
+++ startServers.sh
@@ -2,7 +2,7 @@
 
 # Define an array of session names and corresponding commands
 sessions=("startlds" "startsel fupdate" "startsw" "starts" "starta" "startaw")
-sessions=("npm run startlds" "npm run startselfupdate" "npm run startsw" "npm run starts" "npm run starta" "npm run startaw")
+commands=("npm run startlds" "npm run startselfupdate" "npm run startsw" "npm run starts" "npm run starta" "npm run startaw")
 dircommand=("cd /home/ubuntu/server-folder-for-android")
 
 # Loop through the sessions and start each one
@@ -11,7 +11,7 @@ for ((i=0; i<${#sessions[@]}; i++)); do
   command="${commands[$i]}"
   
   # Start the tmux session
-  tmux new-session -d -s "$session"
+  tmux new-session -s "$session"
   
   # Send the command to the session
   tmux send-keys -t "$session" "$dircommand" C-m
